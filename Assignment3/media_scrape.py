9554ced1304518fd0c70062b962d1108b863c660
Rushikesh Arun Suryagandh <rushisuryagandh4455@gmail.com>
2025-02-06 11:37:26 +0530

    demo commit

---
+++ Assignment3/media_scrape.py
@@ -0,0 +1,24 @@
+import aiohttp
+import requests
+import asyncio
+from bs4 import BeautifulSoup
+def fetch_articles():
+    pass
+
+def scrape_media_channel_1():
+    pass
+
+def scrape_media_channel_2():
+    pass
+
+def main():
+    urls=['https://indianexpress.com/article/cities/chandigarh/deportee-handcuffed-legs-chained-struggled-to-use-washroom-for-40-hrs-9820011/?ref=breaking_hp',
+          'https://www.hindustantimes.com/cricket/india-vs-england-live-score-1st-odi-ind-vs-eng-latest-scorecard-updates-today-february-5-vca-stadium-nagpur-101738780246683.html'
+          ]
+    for url in urls:
+        if 'indianexpress' in url:
+
+    pass
+
+if __name__=='__main__':
+    main()
