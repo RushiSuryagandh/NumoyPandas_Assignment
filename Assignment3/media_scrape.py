d54f6508e84d034f231432f81e14d0f50e63fa1f
Rushikesh Arun Suryagandh <rushikesh.suryagandh@emergys.com>
2025-02-06 11:57:39 +0530

    demo commit

--- Assignment3/media_scrape.py
+++ Assignment3/media_scrape.py
@@ -6,19 +6,17 @@ def fetch_articles():
     pass
 
 def scrape_media_channel_1():
+    url='https://indianexpress.com/article/cities/chandigarh/deportee-handcuffed-legs-chained-struggled-to-use-washroom-for-40-hrs-9820011/?ref=breaking_hp'
     pass
 
 def scrape_media_channel_2():
+    url='https://www.hindustantimes.com/cricket/india-vs-england-live-score-1st-odi-ind-vs-eng-latest-scorecard-updates-today-february-5-vca-stadium-nagpur-101738780246683.html'
     pass
 
 def main():
-    urls=['https://indianexpress.com/article/cities/chandigarh/deportee-handcuffed-legs-chained-struggled-to-use-washroom-for-40-hrs-9820011/?ref=breaking_hp',
-          'https://www.hindustantimes.com/cricket/india-vs-england-live-score-1st-odi-ind-vs-eng-latest-scorecard-updates-today-february-5-vca-stadium-nagpur-101738780246683.html'
-          ]
-    for url in urls:
-        if 'indianexpress' in url:
-
-    pass
+    scrape_media_channel_1()
+    scrape_media_channel_2()
 
 if __name__=='__main__':
     main()
+
